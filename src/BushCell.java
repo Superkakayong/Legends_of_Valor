e98f48da4642a2afb70d808985a3a63f69fad753
Dayong <dayong@bu.edu>
2021-08-25 22:39:33 +0800

    changed BushCell.java

--- src/BushCell.java
+++ src/BushCell.java
@@ -1,6 +1,6 @@
 /**
  * This class represents a bush cell, which belongs to a specific type of map cells. 
- * Therefore, it inherits from the Cell class.
+ * Therefore, it inherits from the Cell class. 
  */
 public class BushCell extends Cell{
     public BushCell(String leftMarker, String rightMarker) {
