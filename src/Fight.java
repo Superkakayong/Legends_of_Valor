6a13faff33de8eb3f4ee6f820d4be47cd1ac7fd3
Dayong <dayong@bu.edu>
2021-08-30 23:06:28 +0800

    modified Fight.java

--- src/Fight.java
+++ src/Fight.java
@@ -2,6 +2,6 @@
  * This interface serves as a contract to the user that if a class implements it, the class should be able to fight.
  * In this project, both the Hero class and Monster class have implemented this interface.
  */
-public interface Fight {
+public interface Fight { 
     void attack();
 }
